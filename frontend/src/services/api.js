351c85ee26ce2809b662834e59e6c74dcdecb584
Manikandan1511 <manikandanmk15112005@gmail.com>
2026-08-04 18:49:29 +0530

    Prepare project for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 //Axios base instance
 //API versioning handled here
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
